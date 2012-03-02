df3cb36e705299ffa4bb1ce88226e7a36bb66079
Jason733i <uniqueberry@gmail.com>
2012-03-02 18:06:31 -0500

    First database migration

---
+++ sql/000_create_player_table.sql
@@ -0,0 +1,5 @@
+create table player (id BIGINT , username VARCHAR(50));
+
+create table version (version INT, component VARCHAR(255));
+INSERT INTO version (version, component) VALUES(000, 'SCHEMA');
+
