69e02e510a6cc6e1fba776b92a448450f96c3133
Jason Berry <uniqueberry@gmail.com>
2012-03-25 17:25:55 -0400

    Plugin now loads when server loads.  Also removing sql migrations that are no longer needed.

--- sql/000_create_player_table.sql
+++
@@ -1,5 +0,0 @@
-create table player (id BIGINT , username VARCHAR(50));
-
-create table version (version INT, component VARCHAR(255));
-INSERT INTO version (version, component) VALUES(000, 'SCHEMA');
-
