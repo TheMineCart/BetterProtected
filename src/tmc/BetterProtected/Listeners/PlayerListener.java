991579b2e1a035c36b6c5bac1dbe714e0bbbbc3f
Jason Berry <uniqueberry@gmail.com>
2012-03-31 17:07:49 -0400

    Adding listener to check BlockEvent information of a liquid type block.

--- src/tmc/BetterProtected/Listeners/PlayerListener.java
+++ src/tmc/BetterProtected/Listeners/PlayerListener.java
@@ -7,11 +7,14 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerEggThrowEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import tmc.BetterProtected.domain.BlockCoordinate;
 import tmc.BetterProtected.domain.BlockEvent;
 import tmc.BetterProtected.domain.World;
 import tmc.BetterProtected.svc.BlockEventRepository;
+
+import static org.bukkit.Material.*;
 
 public class PlayerListener implements Listener {
 
@@ -28,24 +31,43 @@ public class PlayerListener implements Listener {
 
         if (player.isOp() && playerClickedWithStick(player, playerAction)) {
             Block clickedBlock = event.getClickedBlock();
-            BlockEvent protectedBlock = blockEventRepository.findMostRecent(BlockCoordinate.newCoordinate(clickedBlock), World.newWorld(clickedBlock));
-            if (protectedBlock != null) {
+            printBlockEventInformation(player, clickedBlock);
+        }
+    }
 
-                player.sendMessage(ChatColor.RED + String.format("%s at %s, %s, %s was placed by %s on %s at %s",
-                        ChatColor.GOLD + protectedBlock.getMaterial().name() + ChatColor.RED,
-                        ChatColor.DARK_AQUA + protectedBlock.getBlockCoordinate().getX().toString() + ChatColor.RED,
-                        ChatColor.DARK_AQUA + protectedBlock.getBlockCoordinate().getY().toString() + ChatColor.RED,
-                        ChatColor.DARK_AQUA + protectedBlock.getBlockCoordinate().getZ().toString() + ChatColor.RED,
-                        ChatColor.DARK_PURPLE + protectedBlock.getOwner().getUsername() + ChatColor.RED,
-                        ChatColor.GREEN + protectedBlock.getInstant().toString("M/d/yy") + ChatColor.RED,
-                        ChatColor.GREEN + protectedBlock.getInstant().toString("h:mm:ss aa")
-                ));
-            }
+    @EventHandler
+    public void playerThrowsEggAtBlock(PlayerEggThrowEvent event) {
+        Block block = event.getEgg().getLocation().getBlock();
+        Material blockType = block.getType();
+        Player player = event.getPlayer();
+
+        if (player.isOp() && isMaterialLiquid(blockType)) {
+            printBlockEventInformation(player, block);
+        }
+    }
+
+    private void printBlockEventInformation(Player player, Block block) {
+        BlockEvent protectedBlock = blockEventRepository.findMostRecent(BlockCoordinate.newCoordinate(block), World.newWorld(block));
+        if (protectedBlock != null) {
+
+            player.sendMessage(ChatColor.RED + String.format("%s at %s, %s, %s was placed by %s on %s at %s",
+                    ChatColor.GOLD + protectedBlock.getMaterial().name() + ChatColor.RED,
+                    ChatColor.DARK_AQUA + protectedBlock.getBlockCoordinate().getX().toString() + ChatColor.RED,
+                    ChatColor.DARK_AQUA + protectedBlock.getBlockCoordinate().getY().toString() + ChatColor.RED,
+                    ChatColor.DARK_AQUA + protectedBlock.getBlockCoordinate().getZ().toString() + ChatColor.RED,
+                    ChatColor.DARK_PURPLE + protectedBlock.getOwner().getUsername() + ChatColor.RED,
+                    ChatColor.GREEN + protectedBlock.getInstant().toString("M/d/yy") + ChatColor.RED,
+                    ChatColor.GREEN + protectedBlock.getInstant().toString("h:mm:ss aa")
+            ));
         }
     }
 
     private boolean playerClickedWithStick(Player player, Action playerAction) {
-         return player.getItemInHand().getType() == Material.STICK &&
+         return player.getItemInHand().getType() == STICK &&
                  (playerAction == Action.LEFT_CLICK_BLOCK || playerAction == Action.RIGHT_CLICK_BLOCK);
+    }
+
+    private boolean isMaterialLiquid(Material blockType) {
+        return blockType == STATIONARY_WATER || blockType == WATER || blockType == STATIONARY_LAVA || blockType == LAVA;
     }
 }
